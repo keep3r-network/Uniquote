9404aecc2b2de812051d2abf3eb54d55dcc15621
Anton Nell <antonnell.crypto@gmail.com>
2020-11-17 14:30:27 +0200

    Using latest volatility contract. 0xCCdfCB72753CfD55C5afF5d98eA5f9C43be9659d
    Signed-off-by: Anton Nell <antonnell.crypto@gmail.com>

--- src/stores/config/config.js
+++ src/stores/config/config.js
@@ -2,7 +2,7 @@ const config = {
   provider: 'https://mainnet.infura.io/v3/211c5f35ed97405b86465c1d76502922',
   // unioracleAddress: '0x127a2975c4E1c75f1ed4757a861bbd42523DB035',
   keep3rOracleAddress: '0x73353801921417f465377c8d898c6f4c0270282c',
-  keep3rVolatilityAddress: '0x5854a86b67f7ec80dad9083b98758b01049b4532'
+  keep3rVolatilityAddress: '0xCCdfCB72753CfD55C5afF5d98eA5f9C43be9659d'
 };
 
 export default config;
