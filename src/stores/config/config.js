16c75ac555ededa3dfe285bb1d5fda0ca1f1dc9b
Andre Cronje <andre.cronje@yearn.finance>
2021-03-20 08:15:47 +0200

    sushiswap addresses
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- src/stores/config/config.js
+++ src/stores/config/config.js
@@ -1,8 +1,8 @@
 const config = {
   provider: 'https://mainnet.infura.io/v3/211c5f35ed97405b86465c1d76502922',
   // unioracleAddress: '0x127a2975c4E1c75f1ed4757a861bbd42523DB035',
-  keep3rOracleAddress: '0x73353801921417f465377c8d898c6f4c0270282c',
-  keep3rVolatilityAddress: '0xCCdfCB72753CfD55C5afF5d98eA5f9C43be9659d'
+  keep3rOracleAddress: '0xf67Ab1c914deE06Ba0F264031885Ea7B276a7cDa',
+  keep3rVolatilityAddress: '0x173ed6531818456f29Fc74011a3B1FB4B6132Dc9'
 };
 
 export default config;
